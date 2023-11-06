d7d5b475f081210b08119ce666699b964fc1a346
lefex-v2 <146823569+lefex-v2@users.noreply.github.com>
2023-11-06 10:48:28 +0100

    Started on a menu!

---
+++ Card_Games/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+GameLoop gameLoop = new GameLoop();
+List<Action> games = new List<Action>();
+
+games.Add(gameLoop.HigherOrLower);
+
+string[] choice = new string[1] { "1" };
+bool loop = true;
+
+Console.WriteLine("Pick a game to play!\r\n");
+Console.WriteLine("1: Higher or lower!");
+
+while (loop)
+{
+    int i = 0;
+    string d = Console.ReadLine();
+    if(d == choice[i])
+    {
+        games[i](); 
+    }
+    i++;
+}
