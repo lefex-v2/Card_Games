8bf1ff90a84c36ace4660f839b9b41f3f47926b9
lefex-v2 <146823569+lefex-v2@users.noreply.github.com>
2023-11-07 12:01:18 +0100

    Test

---
+++ Card_Games/CardHand.cs
@@ -0,0 +1,19 @@
+﻿class CardHand
+{
+    new List<Cards> cardHand = new List<Cards>();
+
+    public CardHand()
+    {
+        
+    }
+
+    void AddCardToHand(Cards addedCard)
+    {
+        cardHand.Add(addedCard);
+    }
+
+    void RemoveCardFromHand(int cardInx)
+    {
+        cardHand.Remove(cardHand[cardInx]);
+    }
+}
